6221791d675c13d0de322c82b27db2182eaf843f
Jack Bishop <jack.darizat@gmail.com>
2016-07-13 09:46:12 -0700

    Adding soft_delete method to fundingsources api

--- lib/dwolla_swagger/api/fundingsources_api.rb
+++ lib/dwolla_swagger/api/fundingsources_api.rb
@@ -232,6 +232,49 @@ module DwollaSwagger
 
     end
 
+    # Remove a funding source.
+    # 
+    # @param id Funding source ID to remove.
+    # @param [Hash] opts the optional parameters
+    # @option opts [RemoveBankRequest] :body request body to remove a funding source
+    # @return [Unit]
+    def self.soft_delete(id, opts = {})
+
+      # verify the required parameter 'id' is set
+      raise "Missing the required parameter 'id' when calling soft_delete" if id.nil?
+
+      # resource path
+      path = "/funding-sources/{id}".sub('{format}','json')
+
+      # check if id parameter is resource URI, otherwise substitute for ID
+      path = id =~ URI::regexp ? path.sub('{' + 'id' + '}', id.split('/')[-1].to_s) : path.sub('{' + 'id' + '}', id.to_s)
+
+      # query parameters
+      query_params = {}
+
+      # header parameters
+      header_params = {}
+
+      # HTTP header 'Accept' (if needed)
+      _header_accept = ['application/vnd.dwolla.v1.hal+json']
+      _header_accept_result = Swagger::Request.select_header_accept(_header_accept) and header_params['Accept'] = _header_accept_result
+
+      # HTTP header 'Content-Type'
+      _header_content_type = []
+      header_params['Content-Type'] = Swagger::Request.select_header_content_type(_header_content_type)
+
+      # form parameters
+      form_params = {}
+
+      # http body (model)
+      post_body = Swagger::Request.object_to_http_body(opts[:'body'])
+
+      response = Swagger::Request.new(:POST, path, {:params => query_params, :headers => header_params, :form_params => form_params, :body => post_body, :auth_names => @auth_names}).make
+
+      response.code == 201 ? obj = response.headers['Location'] : (obj = Unit.new() and obj.build_from_hash(response.body))
+
+    end
+
     # Delete a funding source by id.
     # 
     # @param id Funding source ID to delete.
